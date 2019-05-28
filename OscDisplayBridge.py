7e4a330f28b7cbbd5002920a81ff67916a1a3950
Giedrius <giedrius@blokas.io>
2019-05-29 02:12:10 +0300

    Nice progress

--- OscDisplayBridge.py
+++ OscDisplayBridge.py
@@ -8,9 +8,15 @@ from pythonosc.osc_server import BlockingOSCUDPServer
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
 
-def find_orac_ctl_port():
-	midiout = rtmidi.MidiOut()
-	available_ports = midiout.get_ports()
+parser = argparse.ArgumentParser()
+parser.add_argument("--ip", default="127.0.0.1", help="The IP of the DisplayOSC server")
+parser.add_argument("--port", type=int, default=6100, help="The port the DisplayOSC server is listening on")
+args = parser.parse_args()
+
+client = udp_client.SimpleUDPClient(args.ip, args.port)
+
+def find_orac_ctl_port(port):
+	available_ports = port.get_ports()
 	i = 0
 	for p in available_ports:
 		if "ORAC-CTL" in p:
@@ -20,21 +26,73 @@ def find_orac_ctl_port():
 	return -1
 
 midiout = rtmidi.MidiOut()
-midiout.open_port(find_orac_ctl_port())
+midiout.open_port(find_orac_ctl_port(midiout))
+
+midiin = rtmidi.MidiIn()
+midiin.open_port(find_orac_ctl_port(midiin))
+
+lines = [("", False)] * 6
+
+def midiin_callback(event, data=None):
+	# 0 1 2     3    4    5
+	# B A Right Down Left Up
+	message = event[0]
+	pressedDown = message[0] == 0x90
+
+	#if not pressedDown:
+	#	return
+
+	value = 127 if pressedDown else 0
+
+	if message[1] == 1:
+		client.send_message("/NavActivate", value)
+	elif message[1] == 3:
+		client.send_message("/NavNext", value)
+	elif message[1] == 5:
+		client.send_message("/NavPrev", value)
+	elif message[1] == 2:
+		client.send_message("/ModuleNext", value)
+	elif message[1] == 4:
+		client.send_message("/ModulePrev", value)
+
+midiin.set_callback(midiin_callback)
+
+def send_text(line, text, inverted):
+	msg = [0xf0, 0x01 if inverted else 0x00, line]
+
+	for c in bytes(text if text else "", encoding='utf-8'):
+		msg.append(c if c <= 0x7f else '_')
+
+	msg.append(0xf7)
+
+	midiout.send_message(msg)
 
 def text_handler(address, *osc_arguments):
-	print(osc_arguments)
-	midiout.send_message([0x90, 0x40, 0x30])
+	print("%d: %s" % (osc_arguments[0], osc_arguments[1]))
+	i = osc_arguments[0]
+	lines[i] = (osc_arguments[1], lines[i][1])
+	send_text(i, lines[i][0], lines[i][1])
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--ip", default="127.0.0.1", help="The IP of the DisplayOSC server")
-parser.add_argument("--port", type=int, default=6100, help="The port the DisplayOSC server is listening on")
-args = parser.parse_args()
+def select_text_handler(address, *osc_arguments):
+	print("select %d" % osc_arguments[0])
+	i = osc_arguments[0]
+	lines[i] = (lines[i][0], not lines[i][1])
+	send_text(i, lines[i][0], lines[i][1])
 
-client = udp_client.SimpleUDPClient(args.ip, args.port)
+def clear_text_handler(address, *osc_arguments):
+	for i in range(6):
+		lines[i] = ("", False)
+		send_text(i, "", False)
+
+def all_other_handler(address, *osc_arguments):
+	print(address, osc_arguments)
 
 dispatcher = Dispatcher()
 dispatcher.map("/text", text_handler)
+dispatcher.map("/selectText", select_text_handler)
+dispatcher.map("/clearText", clear_text_handler)
+dispatcher.map("/*", all_other_handler)
+
 
 #async def loop():
 #	"""Example main loop that only runs for 10 iterations before finishing"""
@@ -42,7 +100,18 @@ dispatcher.map("/text", text_handler)
 #		print(f"Loop {i}")
 #		await asyncio.sleep(1)
 
+client.send_message("/Connect", 6111)
+
 
 #async def init_main():
 server = BlockingOSCUDPServer(('127.0.0.1', 6111), dispatcher)
-server.serve_forever()
+
+try:
+	server.serve_forever()
+finally:
+	print("Cleaning up MIDI.")
+	midiin.close_port()
+	del midiin
+	midiout.close_port()
+	del midiout
+	print("Done!")
--- orac-controller.ino
+++ orac-controller.ino
@@ -1,22 +1,144 @@
 #include <usbmidi.h>
 #include "SH1106.h"
 
+#include "font.h"
+
+uint8_t g_buttonState;
+
 void setup()
 {
 	sh1106_init(SS, PIN_LCD_DC, PIN_LCD_RESET);
+	// Enable pull-ups on button pins.
+	PORTC = 0x3f;
+	g_buttonState = PINC & 0x3f;
+
+	print(0, "Waiting for OscDisplayBridge...", 31, false);
 }
+
+void print(int8_t line, const char *text, uint8_t n, bool inverted)
+{
+	sh1106_set_position(0, 7-(line&7));
+	int16_t space = 128 - n*(FONT_WIDTH+1);
+	while (n--)
+	{
+		const uint8_t *p = &MICRO_FONT[(*text++ - ' ') * FONT_WIDTH];
+		sh1106_draw_progmem_bitmap(p, FONT_WIDTH, inverted);
+		sh1106_draw_space(1, inverted);
+	}
+	if (space > 0)
+	{
+		sh1106_draw_space(space, inverted);
+	}
+}
+
+char g_messageBuffer[128 / 4];
+
+class CommandHandler
+{
+public:
+	CommandHandler()
+		:m_state(STATE_WAITING_FOR_COMMAND)
+		,m_textLine(-1)
+	{
+	}
+
+	void process(uint8_t b)
+	{
+		if (b == 0xf7)
+		{
+			executeCommand();
+			m_state = STATE_WAITING_FOR_COMMAND;
+			return;
+		}
+
+		switch (m_state)
+		{
+		case STATE_WAITING_FOR_COMMAND:
+			if (b == 0xf0)
+			{
+				m_state = STATE_SYSEX_START;
+			}
+			return;
+		case STATE_SYSEX_START:
+			if (b == 0x00)
+			{
+				m_state = STATE_RECEIVING_TEXT;
+				m_textLine = -1;
+				m_counter = 0;
+			}
+			else if (b == 0x01)
+			{
+				m_state = STATE_RECEIVING_INVERTED_TEXT;
+				m_textLine = -1;
+				m_counter = 0;
+			}
+			else m_state = STATE_WAITING_FOR_COMMAND;
+			return;
+		case STATE_RECEIVING_TEXT:
+		case STATE_RECEIVING_INVERTED_TEXT:
+			if (m_textLine == -1)
+			{
+				m_textLine = b;
+			}
+			else
+			{
+				if (m_counter < sizeof(g_messageBuffer))
+					g_messageBuffer[m_counter++] = b;
+			}
+			return;
+		}
+	}
+
+private:
+	void executeCommand()
+	{
+		if (m_state == STATE_RECEIVING_TEXT || m_state == STATE_RECEIVING_INVERTED_TEXT)
+		{
+			print(m_textLine, g_messageBuffer, m_counter, m_state == STATE_RECEIVING_INVERTED_TEXT);
+		}
+	}
+
+	enum State
+	{
+		STATE_WAITING_FOR_COMMAND,
+		STATE_SYSEX_START,
+		STATE_RECEIVING_TEXT,
+		STATE_RECEIVING_INVERTED_TEXT,
+		STATE_SYSEX_END,
+	};
+
+	State m_state;
+	int8_t m_textLine;
+	uint8_t m_counter;
+};
+
+CommandHandler g_handler;
 
 void loop()
 {
 	while (USBMIDI.available())
 	{
-		if ((USBMIDI.read() & 0xf0) == 0x90)
+		g_handler.process(USBMIDI.read());
+	}
+
+	uint8_t newButtonState = PINC & 0x3f;
+	uint8_t change = newButtonState ^ g_buttonState;
+
+	if (change)
+	{
+		for (int8_t i=0; i<6; ++i)
 		{
-			static bool x = false;
-			sh1106_set_position(10, 2);
-			sh1106_draw_space(4, x);
-			x = !x;
+			int8_t bit = 1 << i;
+			if (change & bit)
+			{
+				bool on = !(newButtonState & bit);
+
+				USBMIDI.write(on ? 0x90 : 0x80);
+				USBMIDI.write(i);
+				USBMIDI.write(on ? 100 : 0);
+			}
 		}
+		g_buttonState = newButtonState;
 	}
 
 	USBMIDI.poll();
