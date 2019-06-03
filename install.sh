17c58c8a7bc92d725030d91816529574d0b73f17
Giedrius <giedrius@blokas.io>
2019-06-04 01:16:57 +0300

    Install script

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-sudo apt install python3-pip libjack-jackd2-dev
-sudo pip3 install python-rtmidi python-osc
+
+if [ ! "$EUID" = "0"]; then
+	echo "Please run using 'sudo ./install.sh'"
+	exit 1
+fi
+
+BASE_PATH=$(dirname $(readlink -f $0))
+
+apt install python3-pip libjack-jackd2-dev
+pip3 install python-rtmidi python-osc
+install -m 644 $BASE_PATH/orac-bridge/99-orac-ctl-bridge.rules /etc/udev/rules.d/
+install -m 644 $BASE_PATH/orac-bridge/orac-ctl-bridge.service /usr/lib/systemd/system/
+systemctl daemon-reload
